7fdb3aebbf3d1afed8293188e6ee8caf57580d64
Aymeric HINDERCHIETTE <aymhinder@gmail.com>
2021-12-14 23:36:17 +0100

    Add files via upload

--- testaClasse.php
+++ testaClasse.php
@@ -1,16 +1,30 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+   <meta charset="utf-8">
+   <title>Calcul IMC</title>
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+   <div class="container light-red">
+   <center>
+
 <?php
-include 'imc.class.php';
+//include 'imc.class.php';
 $calcul = new CalculIMC();
 
 $taille = $calcul->test_input($_POST['Taille']); 
-$poids = $calcul->test_input( $_POST['Poids']);
+$poids = $calcul->test_input($_POST['Poids']);
 
 echo $calcul->CalcIMC($poids, $taille);
 
 class CalculIMC
 {
-    public $taille = 1;
-    public $poids = 0;
+    public $taille; // Création objet taille
+    public $poids; // Création objet poids
     
    function test_input($data) {
         $data = trim($data);
@@ -23,8 +37,11 @@ class CalculIMC
     function CalcIMC($poids,$taille)
     {
            
-        $imc = ($poids / (pow($taille,2)));
+        $imc = ($poids / (pow($taille,2))); // Formule de calcul IMC
+        echo '<div class="container light-red">';
         echo "Votre IMC est à ".$imc;
+
+        // Résultat du test IMC
 
         if ($imc > 40){
             echo "<br>Vous êtes en obésité morbide";
@@ -41,9 +58,13 @@ class CalculIMC
         }else{
             echo "<br>Vous êtes en dénutrition";
         }
+    
+    echo "</div>";
 
-        
-        //return $imc;
     }
 
 }
+?>
+</center>
+</body>
+</html>
