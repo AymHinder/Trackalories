f478f7678e952706f610d4f7be7831b250fd5fa4
Aymeric HINDERCHIETTE <aymhinder@gmail.com>
2021-12-15 00:06:59 +0100

    Add files via upload

--- testaClasse.php
+++ testaClasse.php
@@ -10,61 +10,63 @@
 <body>
 
    <div class="container light-red">
+   <div class="m-auto">
    <center>
 
-<?php
-//include 'imc.class.php';
-$calcul = new CalculIMC();
+    <?php
+    //include 'imc.class.php';
+    $calcul = new CalculIMC();
 
-$taille = $calcul->test_input($_POST['Taille']); 
-$poids = $calcul->test_input($_POST['Poids']);
+    $taille = $calcul->test_input($_POST['Taille']); 
+    $poids = $calcul->test_input($_POST['Poids']);
 
-echo $calcul->CalcIMC($poids, $taille);
+    echo $calcul->CalcIMC($poids, $taille);
 
-class CalculIMC
-{
-    public $taille; // Création objet taille
-    public $poids; // Création objet poids
-    
-   function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-    
-    
-    function CalcIMC($poids,$taille)
+    class CalculIMC
     {
-           
-        $imc = ($poids / (pow($taille,2))); // Formule de calcul IMC
-        echo '<div class="container light-red">';
-        echo "Votre IMC est à ".$imc;
-
-        // Résultat du test IMC
-
-        if ($imc > 40){
-            echo "<br>Vous êtes en obésité morbide";
-        }elseif ($imc > 35){
-            echo "<br>Vous êtes en obésité sévère";
-        }elseif ($imc > 30){
-            echo "<br>Vous êtes en obésité modérée";
-        }elseif ($imc > 25){
-            echo "<br>Vous êtes en surpoids";
-        }elseif ($imc > 18.5){
-            echo "<br>Vous êtes à votre poids normal";
-        }elseif ($imc > 16.5){
-            echo "<br>Vous êtes en maigreur";
-        }else{
-            echo "<br>Vous êtes en dénutrition";
+        public $taille; // Création objet taille
+        public $poids; // Création objet poids
+        
+    function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
         }
-    
-    echo "</div>";
+        
+        
+        function CalcIMC($poids,$taille)
+        {
+            
+            $imc = ($poids / (pow($taille,2))); // Formule de calcul IMC
+            echo "<h1>Votre bilan : </h1>";
+            echo "<br><h5>Votre IMC est à ".$imc;
+            echo "</h5>";
+
+            // Résultat du test IMC
+
+            if ($imc > 40){
+                echo "<br><h5>Vous êtes en obésité morbide</h5>";
+            }elseif ($imc > 35){
+                echo "<br><h5>Vous êtes en obésité sévère</h5>";
+            }elseif ($imc > 30){
+                echo "<br><h5>Vous êtes en obésité modérée</h5>";
+            }elseif ($imc > 25){
+                echo "<br><h5>Vous êtes en surpoids</h5>";
+            }elseif ($imc > 18.5){
+                echo "<br><h5>Vous êtes à votre poids normal</h5>";
+            }elseif ($imc > 16.5){
+                echo "<br><h5>Vous êtes en maigreur</h5>";
+            }else{
+                echo "<br><h5>Vous êtes en dénutrition</h5>";
+            }
+
+        }
 
     }
-
-}
-?>
-</center>
+    ?>
+    </center>
+    </div>
+    </div>
 </body>
 </html>
